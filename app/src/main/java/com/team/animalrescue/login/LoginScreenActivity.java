d4072545b843bffcc209daefa05cd25be48b3a1e
uditbhaskar <bhaskarudit8@gmail.com>
2019-12-17 00:29:09 +0530

    Added editText for phone number with country picker.

---
+++ app/src/main/java/com/team/animalrescue/login/LoginScreenActivity.java
@@ -0,0 +1,18 @@
+package com.team.animalrescue.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.team.animalrescue.R;
+import com.team.animalrescue.common.BaseActivity;
+
+public class LoginScreenActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+
+}
